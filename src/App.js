c23c077cc8f84b01d9deeb04af6ebce9c6cb7964
Ferdaws-ksiksi <85398562+Ferdaws-ksiksi@users.noreply.github.com>
2021-08-07 20:52:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import {Component} from 'react';
+import './App.css';
+import  React  from 'react';
+
+class  App extends React.Component {
+    state ={
+    fullName :'Ferdaws',
+    bio:'IAG',
+    imgSrc :"/images/me.jpg",
+    imgAlt:'my picture',
+    profession:'Student',
+    show : false,
+    timer :0
+    };
+  handleClick=e=>{
+    e.preventDefault();
+    this.setState({...this.state,
+      show:!this.state.show})
+  }
+  Increment = ()=>{
+    this.setState(
+      {...this.state,timer:this.state.timer+1}
+    )
+  }
+  componentDidMount(){
+    setInterval(this.Increment,1000)
+  }
+  render(){
+  return (
+    <div className="App" > 
+    <button style={{
+      marginTop:'5%',
+      height:'30px',
+      backgroundColor:'pink',
+      border:'none',
+      cursor:'pointer'
+    }}
+    onClick={this.handleClick}>button</button> 
+    {this.state.show==true?<div style={{}}>
+      <p>My name : {this.state.fullName}</p>
+      <p>and I am : {this.state.profession}</p>
+      <p>timer : {this.state.timer}</p>
+      <img alt={this.state.imgAlt} src={this.state.imgSrc} 
+      style={{
+        height:'200px',width:'200px'
+      }}/>
+    </div>:<div></div>}    
+    </div>
+    
+  );}
+}
+
+export default App;
